437b020bbc1ee55a6fd552580274d3af65a6e395
Singhmansingh <shavinsingh1999@gmail.com>
2023-06-14 19:44:28 -0400

    internal system API

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\ScriptsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use App\Models\ScheduledSegment;
+use App\Models\Segment;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -38,9 +41,45 @@ Route::get('/audio/scripts',function(Request $request) {
     $scheduledsegments=ScheduledSegment::with('script')->with('script.segment')->get();
     $playlist='';
     foreach($scheduledsegments as $ss){
-        $playlist.="http://192.168.68.119/storage/audio/20.mp3\r\n";
+        $playlist.="https://storage/audio/20.mp3\r\n";
     }
 
     return $playlist;
 });
 
+Route::post('/system/add',function(Request $req) {
+    /**
+     * {
+     *  "prompt": "Hello this is a prompt",
+     *  "segment_title": "GPS update June 2023",
+     *  "internal_system_id":3,
+     * 
+     * }
+     * 
+     */
+    $req->validate([
+        "prompt"=>"required",
+        "segment_title"=>"required",
+        "internal_system_id"=>"required|exists:internal_systems,id",
+    ]);
+
+     $prompt = $req->input('prompt');
+     $segmenttitle = $req->input('segment_title');
+     $internalsystemid = $req->input('internal_system_id');
+
+     $segment = new Segment();
+     $segment->title=$segmenttitle;
+     $segment->segment_data= json_encode(array("prompt"=>$prompt));
+     $segment->internal_system_id = $internalsystemid;
+     $segment->segment_type_id = 1;
+     $segment->user_id = 1;
+
+     $segment->save();
+
+     $segmentid = $segment->id;
+
+     
+
+     return $segment;
+
+});
